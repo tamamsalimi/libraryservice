ac83bcc8b8078faded70d4334b29a57b1ddf90e0
tamamindico <tamam.salimi@indicocompany.com>
2026-01-06 16:23:37 +0700

    update initial password

--- src/main/resources/db/migration/V1_2__Insert_initial_user.sql
+++ src/main/resources/db/migration/V1_2__Insert_initial_user.sql
@@ -1,7 +1,7 @@
 INSERT INTO users (username, password, role, member_id)
 VALUES (
            'admin',
-           '$2a$10$7q3K6qYJ8J5e6s6n6Z7s.uF6M8O2xZ6Z6wZpQeR6yZ6n6qX6Y',
+           '$2a$10$j05V.jr.0JuXoZpiyRzvke3KlOICEIYGt1SgfEaYx6nJBk4H6XWam',
            'ADMIN',
            NULL
        );
